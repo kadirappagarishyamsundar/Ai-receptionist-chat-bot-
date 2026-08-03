954b672ae8974d5039a3761f661350b80ec9072a
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-08-03 17:06:56 +0530

    modified the backend

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__, static_folder='../frontend', static_url_path='')
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
 
 
